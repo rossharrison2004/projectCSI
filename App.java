ab1733c5fd9954a1c2ffef6f8dc45a30d7692f8d
rossharrison2004 <147664321+rossharrison2004@users.noreply.github.com>
2023-11-30 11:44:33 -0500

    Update App.java

--- App.java
+++ App.java
@@ -19,10 +19,13 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class App extends Application {
+    // TextFields for user input and displaying guessed letters
     private TextField tfGuess = new TextField();
     private TextField tfLettersGuessed = new TextField();
+    // Instance of the Dictionary class to get a hidden word
     private Dictionary word = new Dictionary();
 
+    // Arrays and variables to manage the game state
     private Text[] text;
     private Label guessesRemaining;
     private Label resultLabel;
@@ -33,8 +36,10 @@ public class App extends Application {
     private StringBuilder guessedLetters;
     private String theWord;
 
+    // Flag to check if the game has ended
     private boolean gameEnded = false;
 
+    // Initialize the gallows components
     private void initGallows() {
         Line gallow1 = new Line(25, 25, 200, 25);
         gallow1.setStroke(Color.RED);
@@ -57,9 +62,11 @@ public class App extends Application {
         children.add(rope);
     }
 
+    // Initialize the body parts of the hangman
     private void initBody() {
         body = new ArrayList<>();
 
+        // Head
         Ellipse head = new Ellipse(200, 112, 35, 35);
         head.setStroke(Color.BLACK);
         head.setFill(Color.WHITE);
@@ -68,6 +75,7 @@ public class App extends Application {
         children.add(head);
         body.add(head);
 
+        // Torso
         Line tor = new Line(200, 200, 200, 150);
         tor.setStroke(Color.BLACK);
         tor.setStrokeWidth(5);
@@ -75,6 +83,7 @@ public class App extends Application {
         children.add(tor);
         body.add(tor);
 
+        // Left Arm
         Line lftArm = new Line(150, 225, 200, 175);
         lftArm.setStroke(Color.BLACK);
         lftArm.setStrokeWidth(5);
@@ -82,6 +91,7 @@ public class App extends Application {
         children.add(lftArm);
         body.add(lftArm);
 
+        // Right Arm
         Line rtArm = new Line(250, 225, 200, 175);
         rtArm.setStroke(Color.BLACK);
         rtArm.setStrokeWidth(5);
@@ -89,6 +99,7 @@ public class App extends Application {
         children.add(rtArm);
         body.add(rtArm);
 
+        // Left Leg
         Line lftLeg = new Line(200, 200, 175, 275);
         lftLeg.setStroke(Color.BLACK);
         lftLeg.setStrokeWidth(5);
@@ -96,6 +107,7 @@ public class App extends Application {
         children.add(lftLeg);
         body.add(lftLeg);
 
+        // Right Leg
         Line rtLeg = new Line(200, 200, 225, 275);
         rtLeg.setStroke(Color.BLACK);
         rtLeg.setStrokeWidth(5);
@@ -104,6 +116,7 @@ public class App extends Application {
         body.add(rtLeg);
     }
 
+    // Initialize blank spaces for the hidden word
     private void initBlanks(String word) {
         Line[] blanks = new Line[word.length()];
         int xStart = 375;
@@ -118,6 +131,7 @@ public class App extends Application {
         }
     }
 
+    // Initialize Text elements for displaying hidden word
     private Text[] initText(String word) {
         Text[] text = new Text[word.length()];
         int xStartw = 355;
@@ -134,23 +148,29 @@ public class App extends Application {
         return text;
     }
 
+    // Application start method
     @Override
     public void start(Stage primaryStage) {
+        // Create a Pane to hold all components
         Pane pane = new Pane();
         children = pane.getChildren();
 
+        // Initialize gallows and hangman body parts
         initGallows();
         initBody();
 
+        // Get a hidden word for the game
         theWord = word.getHiddenWord().toUpperCase();
         guessedLetters = new StringBuilder();
 
+        // Initialize blank spaces for the hidden word
         initBlanks(theWord);
 
-
+        // Initialize Text elements for displaying hidden word
         text = initText(theWord);
         left = 6;
 
+        // Create a GridPane for user input and game information
         GridPane gridPane = new GridPane();
         gridPane.setHgap(5);
         gridPane.setVgap(5);
@@ -159,56 +179,64 @@ public class App extends Application {
         gridPane.add(new Label("Letters Guessed:"), 0, 1);
         gridPane.add(tfLettersGuessed, 1, 1);
         gridPane.add(new Label("Guesses Remaining: "), 0, 2);
+
+        // Label for displaying remaining guesses
         guessesRemaining = new Label(String.valueOf(left));
         gridPane.add(guessesRemaining, 0, 3);
 
+        // Label for displaying game result
         resultLabel = new Label("");
         gridPane.add(resultLabel, 1, 4);
 
+        // Create a BorderPane to organize components
         BorderPane thing = new BorderPane();
         thing.setRight(gridPane);
         thing.setCenter(pane);
 
+        // Button to start a new game
         Button resetButton = new Button("Start New Game");
         resetButton.setOnAction(e -> resetGame());
         gridPane.add(resetButton, 1, 5);
         GridPane.setHalignment(resetButton, HPos.RIGHT);
 
+        // Event handler for user input
         tfGuess.setOnAction(e -> playGame());
 
+        // Create the main Scene
         Scene scene = new Scene(thing, 700, 400);
         primaryStage.setTitle("Hangman Game");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
+    // Reset the game to start a new one
     private void resetGame() {
         // Reset all game variables
         theWord = word.getHiddenWord().toUpperCase();
         guessedLetters.setLength(0);
         tfGuess.clear();
         tfLettersGuessed.clear();
-    
+
         // Make all body parts invisible again
         for (Shape part : body) {
             part.setVisible(false);
         }
-    
+
         // Clear existing blanks and text elements
         children.removeIf(node -> node instanceof Line || node instanceof Text);
-    
+
         // Redraw gallows and rope
         initGallows();
         initBlanks(theWord);
         text = initText(theWord);
-    
+
         left = 6;
         guessesRemaining.setText(String.valueOf(left));
         resultLabel.setText("");
         gameEnded = false;
     }
-    
 
+    // Process user input and update game state
     private void playGame() {
         if (gameEnded) {
             System.out.println("The game has ended. Please start a new game.");
@@ -224,6 +252,7 @@ public class App extends Application {
         guess = guess.toUpperCase();
         tfGuess.clear();
 
+        // Check if the letter has already been guessed
         if (guessedLetters.length() > 0) {
             if (guessedLetters.indexOf(guess) > -1) {
                 return;
@@ -235,6 +264,7 @@ public class App extends Application {
         }
         tfLettersGuessed.setText(guessedLetters.toString());
 
+        // Check if the guessed letter is in the hidden word
         boolean good = false;
         for (int i = 0; i < theWord.length(); i++) {
             if (guess.equalsIgnoreCase(theWord.substring(i, i + 1))) {
@@ -243,17 +273,20 @@ public class App extends Application {
             }
         }
 
+        // If the guessed letter is not in the word, reveal a body part
         if (!good) {
             body.get(6 - left).setVisible(true);
             left--;
             guessesRemaining.setText(String.valueOf(left));
         }
 
+        // If no guesses are remaining, end the game and display the correct word
         if (left == 0) {
             resultLabel.setText("Sorry, you lost! The word was: " + theWord);
             gameEnded = true;
         }
 
+        // Check if the word has been completely guessed
         boolean solved = true;
         for (int i = 0; i < text.length; i++) {
             if (!text[i].isVisible()) {
@@ -262,23 +295,23 @@ public class App extends Application {
             }
         }
 
+        // If the word has been guessed, end the game and display a congratulatory message
         if (solved) {
             resultLabel.setText("Congratulations! You guessed the word: " + theWord);
             gameEnded = true;
         }
     }
 
+    // Main method to launch the application
     public static void main(String[] args) {
         Application.launch(args);
     }
 }
 
+// Dictionary class to get a hidden word for the game
 class Dictionary {
     public String getHiddenWord() {
-    String[] words = {"hangman", "java", "programming", "computer", "developer", "oakland", "university", "school", "coding", "engineering", "object", "class", "method", "float"};        return words[(int) (Math.random() * words.length)];
-    return words[(int) (Math.random() * words.length)];
+        String[] words = {"hangman", "java", "programming", "computer", "developer"};
+        return words[(int) (Math.random() * words.length)];
     }
-}
-    }
-    
 }
